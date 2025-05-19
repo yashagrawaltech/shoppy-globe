5b0ec5a645f564506e6e8d04d981ec83ac1f0a9b
yash agrawal <yashagrawaltech24@gmail.com>
2025-05-19 22:59:24 +0530

    Fix Grid Layout

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -3,7 +3,7 @@ import ProductCard from './ProductCard';
 
 const ProductList = ({ productData = [] }) => {
     return (
-        <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 md:gap-4">
+        <div className="w-full grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 md:gap-4">
             {productData && productData.length
                 ? productData.map((p) => {
                       return <ProductCard key={p.id} data={p} />;
